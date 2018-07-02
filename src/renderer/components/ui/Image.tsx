3311a5ca5183d5a79a417792c64f37aebb83945b
Stefan Malzner <stefan@adlk.io>
2018-07-02 14:52:19 +0200

    Work around electron-webpack __static issue

--- src/renderer/components/ui/Image.tsx
+++ src/renderer/components/ui/Image.tsx
@@ -7,8 +7,9 @@ interface IProps extends React.HTMLAttributes<HTMLImageElement> {
 
 export class Image extends React.Component<IProps> {
   public render() {
-
-    const src = path.join('file://', __static, 'images', this.props.src);
+    // work around electron-webpack __static path issue
+    const staticPath = process.env.NODE_ENV === 'production' ? path.join(process.resourcesPath || '', 'static') : __static;
+    const src = path.join('file://', staticPath, 'images', this.props.src);
 
     return (
       <img {...this.props} src={src} />
